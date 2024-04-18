60f4de20f1a9525787f4980bf628300105a625a6
Kevin Olivella <kevinolivella@gmail.com>
2024-04-17 21:25:57 -0500

    Se le realza modificación al ejercicio 2 nuevamente

--- ejercicio2/src/main/java/com/example/Main.java
+++ ejercicio2/src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,29 +11,29 @@ public class Main {
     }
 
     public static void adivinarNumero(int min_numero, int max_numero, int max_intentos) {
-        Random random = new Random();
-        int aleatorio = random.nextInt(max_numero - min_numero + 1) + min_numero;
-        Scanner scanner = new Scanner(System.in);
+          Random random = new Random();
+          int aleatorio = random.nextInt(max_numero - min_numero + 1) + min_numero;
+          Scanner scanner = new Scanner(System.in);
+          
+          System.out.println("Por favor ingresa un número para adivinarlo, entre " + min_numero + " y " + max_numero + "  en " + max_intentos + " intentos. ");
+          int intentos = 0;
+          while (intentos < max_intentos) {
+            System.out.println("Te quedan " + (max_intentos - intentos) + " intentos, para adivinar el número. Ingresa el número.");
+            int numero = scanner.nextInt();
+            if (numero == aleatorio) {
+                System.out.println("¡Felicidades! Adivinaste el número.");
+                return;                
+            } else if (numero < aleatorio) {
+                System.out.println("El número es mayor que " + numero + ". Por favor sigue intentando.");
+                
+            } else {
+                System.out.println("El numero es menor que " + numero + ". Por favor sigue intentando.");
+            }
+            intentos++;
+            
+          } 
+          System.out.println("Lo siento, ¡PERDISTE! Se te acabaron los intentos. Ya que el número era " + aleatorio);
 
-        System.out.println("Ingresa el número que vas a adivinar entre " + min_numero + " y " + max_numero + " en " + max_intentos);
-        int intentos = 0;
-
-        while (intentos < max_intentos) {
-System.out.println("Te quedan " + (max_intentos - intentos) + " Intentos para adivinar el número. Ingresa el número:");
-int numero = scanner.nextInt();
-if (numero == aleatorio) {
-    System.out.println("¡Felicidades. Has adivinado el número.");
-    return;
-    
-} else if (numero < aleatorio) {
-    System.out.println("El número es mayor que " + numero + ". Por favor ingresa otro número.");
-    
-}else {
-    System.out.println("El número es menor que " + numero + ". Por favor ingresa otro número. ");
-}
-      intentos++;      
-        }
-System.out.println("Lo siento. Se te acabaron los intentos. El número era: " + aleatorio);
         
     }
 }
